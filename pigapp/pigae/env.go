79e9a977ae2ee0ba24f3ecacee45b9f2cce78c91
Deleplace <deleplace2015@gmail.com>
2015-08-04 14:38:51 +0200

    Fix absolute URL.

--- pigapp/pigae/env.go
+++ pigapp/pigae/env.go
@@ -19,7 +19,7 @@ type Env struct {
 
 var envProd = Env{
 	IsDev:           false,
-	Host:            "http://www.programming-idioms.org",
+	Host:            "//database-idioms.appspot.com",
 	UseAbsoluteUrls: true,
 	UseMinifiedCss:  true,
 	UseMinifiedJs:   true,
