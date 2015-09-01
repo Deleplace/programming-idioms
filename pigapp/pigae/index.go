32dfbc5a9f15ca913ef32083b271a1dd5081edfe
Deleplace <deleplace2015@gmail.com>
2015-09-01 14:02:57 +0200

    Updated CSS.

--- pigapp/pigae/index.go
+++ pigapp/pigae/index.go
@@ -16,7 +16,7 @@ const ThemeVersion = "default"
 
 // ThemeDate is the prefix used for "revving" the static files and enable long-term HTTP cache.
 // It MUST end with underscore _ (see app.yaml)
-const ThemeDate = "20150827_"
+const ThemeDate = "20150901_"
 
 var r = mux.NewRouter()
 
