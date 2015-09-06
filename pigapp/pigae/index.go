60ac3e8fcccafe8fe4c806a4a2b631d416c88f1d
Deleplace <deleplace2015@gmail.com>
2015-09-06 18:09:12 +0200

    Issue #28: Use monospaced font in forms code blocks.

--- pigapp/pigae/index.go
+++ pigapp/pigae/index.go
@@ -16,7 +16,7 @@ const ThemeVersion = "default"
 
 // ThemeDate is the prefix used for "revving" the static files and enable long-term HTTP cache.
 // It MUST end with underscore _ (see app.yaml)
-const ThemeDate = "20150901_"
+const ThemeDate = "20150906_"
 
 var r = mux.NewRouter()
 
