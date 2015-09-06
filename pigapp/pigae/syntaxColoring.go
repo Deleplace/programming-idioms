023dbb9a401b09d090beec035b82d23393546dfb
Deleplace <deleplace2015@gmail.com>
2015-09-06 20:06:12 +0200

    Issue #34: Fix coloring for Scheme, Haskell, Clojure.

--- pigapp/pigae/syntaxColoring.go
+++ pigapp/pigae/syntaxColoring.go
@@ -12,19 +12,22 @@ type PrettyAdaptor struct {
 
 // Pretty maps some languages to a syntax coloring JS script.
 var Pretty = map[string]PrettyAdaptor{
-	"python": {"py", ""},
-	"ruby":   {"rb", ""},
-	"csharp": {"cs", ""},
-	"go":     {"go", "lang-go.js"},
-	"basic":  {"basic", "lang-basic.js"},
-	"dart":   {"dart", "lang-dart.js"},
-	"erlang": {"erlang", "lang-erlang.js"},
-	"lisp":   {"lisp", "lang-lisp.js"},
-	"lua":    {"lua", "lang-lua.js"},
-	"matlab": {"matlab", "lang-matlab.js"},
-	"pascal": {"pascal", "lang-pascal.js"},
-	"r":      {"r", "lang-r.js"},
-	"scala":  {"scala", "lang-scala.js"},
+	"python":  {"py", ""},
+	"ruby":    {"rb", ""},
+	"csharp":  {"cs", ""},
+	"go":      {"go", "lang-go.js"},
+	"basic":   {"basic", "lang-basic.js"},
+	"dart":    {"dart", "lang-dart.js"},
+	"erlang":  {"erlang", "lang-erlang.js"},
+	"lisp":    {"lisp", "lang-lisp.js"},
+	"lua":     {"lua", "lang-lua.js"},
+	"matlab":  {"matlab", "lang-matlab.js"},
+	"pascal":  {"pascal", "lang-pascal.js"},
+	"r":       {"r", "lang-r.js"},
+	"scala":   {"scala", "lang-scala.js"},
+	"scheme":  {"scm", "lang-lisp.js"},
+	"haskell": {"hs", "lang-hs.js"},
+	"clojure": {"clj", "lang-clj.js"},
 }
 
 func prettifyCSSClass(lang string) string {
