e0a59dbf786fa553b39ac3efb741d307df4ce1e7
Deleplace <deleplace2015@gmail.com>
2015-11-01 23:25:04 +0100

    New efforts to block spam.

--- pigapp/pigae/spam.go
+++ pigapp/pigae/spam.go
@@ -16,32 +16,56 @@ import (
 var spammerSet = map[string]bool{}
 
 func init() {
-	b, err := ioutil.ReadFile("spammers.csv")
+	b1, err := ioutil.ReadFile("spammers.csv")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		return
 	}
-	full := string(b)
-	spammers := strings.Split(full, ",")
-	spammerSet = make(map[string]bool, len(spammers))
-	for _, spammer := range spammers {
-		spammerSet[spammer] = true
+	b2, err := ioutil.ReadFile("spammers-custom.csv")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
 	}
-	for _, ip := range os.Args[1:] {
-		fmt.Println(ip, ":", spammerSet[ip])
+	spammerSet = make(map[string]bool)
+	for _, b := range [][]byte{b1, b2} {
+		full := string(b)
+		spammers := strings.Split(full, ",")
+		for _, spammer := range spammers {
+			spammerSet[spammer] = true
+		}
 	}
 	// How to log this at startup ...?
 	// fmt.Println("Registered list of", len(spammerSet), "spam IPs.")
 }
 
-func isSpam(w http.ResponseWriter, r *http.Request) bool {
+func isSpam(w http.ResponseWriter, r *http.Request) (busted bool) {
+	var motive string
 	ip := r.RemoteAddr
+
+	defer func() {
+		if busted {
+			c := appengine.NewContext(r)
+			c.Infof("Detected spammer %v : %v", ip, motive)
+			// Let's return a nice 200 ... nothing to see here
+			fmt.Fprintln(w, "<html><body>This site is under construction</body></html>")
+		}
+	}()
+
+	// By suspicious IP
 	if spammerSet[ip] {
-		c := appengine.NewContext(r)
-		c.Infof("Detected spammer %v", ip)
-		// Let's return a nice 200 ... nothing to see here
-		fmt.Fprintln(w, "<html><body>This site is under construction</body></html>")
+		motive = "blacklisted"
 		return true
 	}
+
+	// By suspicious field values
+	if len(r.FormValue("impl_imports")) > 150 {
+		motive = "imports too long"
+		return true
+	}
+	if len(r.FormValue("impl_imports")) > 10 && r.FormValue("impl_imports") == r.FormValue("impl_code") {
+		motive = "identical imports and codeblock"
+		return true
+	}
+
 	return false
 }
