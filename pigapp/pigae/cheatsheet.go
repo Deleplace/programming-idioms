691d33964739570fb774f8abbf5ad02ca3fd0496
Deleplace <deleplace2015@gmail.com>
2016-05-28 21:58:42 +0200

    Cheatsheet handle multiple impls without stuttering.

--- pigapp/pigae/cheatsheet.go
+++ pigapp/pigae/cheatsheet.go
@@ -36,6 +36,16 @@ func cheatsheet(w http.ResponseWriter, r *http.Request) error {
 		return PiError{err.Error(), http.StatusInternalServerError}
 	}
 
+	// Don't repeat idiom ID, title, lead on consecutive rows.
+	// Note: this *may* not play well with the JS text filter.
+	for i := 1; i < len(cheatsheetLines); i++ {
+		if cheatsheetLines[i].IdiomID == cheatsheetLines[i-1].IdiomID {
+			cheatsheetLines[i].IdiomID = ""
+			cheatsheetLines[i].IdiomTitle = ""
+			cheatsheetLines[i].IdiomLeadParagraph = "Alternative implementation"
+		}
+	}
+
 	data := CheatSheetFacade{
 		PageMeta: PageMeta{
 			PageTitle: printNiceLang(lang) + " cheat sheet",
