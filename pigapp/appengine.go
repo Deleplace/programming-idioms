334e50418d50cb980538fecba0d2feb3a07ec1f0
deleplace <deleplace@google.com>
2018-12-20 16:10:29 +0100

    Use env var GOOGLE_CLOUD_PROJECT instead of hard-coded value.

--- pigapp/appengine.go
+++ pigapp/appengine.go
@@ -1,13 +1,19 @@
 package main
 
-import "google.golang.org/appengine"
+import (
+	"os"
+
+	"google.golang.org/appengine"
+)
 
 // This ID is needed to create a global Datastore client.
 //
 // It is also used for other concerns (e.g. Cloud Tasks) but
 // could be dynamically derived from http.Request.Context().
 //
-const projectID = "pi-go111"
+var projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
+
+//const projectID = "pi-go111"
 
 func isAppengineDevServer() bool {
 	return appengine.IsDevAppServer()
