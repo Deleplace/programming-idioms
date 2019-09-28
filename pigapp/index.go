0699ae853c96f160a7beb8042f6f1d1a4c46543d
Deleplace <deleplace2015@gmail.com>
2019-09-28 18:35:37 +0200

    Fix stale CSS problem, by bumping folder name once again.

--- pigapp/index.go
+++ pigapp/index.go
@@ -18,7 +18,7 @@ const ThemeVersion = "default"
 
 // ThemeDate is the prefix used for "revving" the static files and enable long-term HTTP cache.
 // It MUST end with underscore _ (see app.yaml)
-const ThemeDate = "20190927_"
+const ThemeDate = "20190928_"
 
 var r = mux.NewRouter()
 
