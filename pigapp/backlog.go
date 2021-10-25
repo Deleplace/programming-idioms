c98db7edbceac9ccd082f049c36072b13f631c9a
Deleplace <deleplace2015@gmail.com>
2021-10-25 19:29:07 +0200

    issues/163 Community backlogs: language logos Dart, Elixir, Groovy.

--- pigapp/backlog.go
+++ pigapp/backlog.go
@@ -121,7 +121,7 @@ func languageLogo(lang string) string {
 	switch lg := strings.TrimSpace(strings.ToLower(lang)); lg {
 	case "csharp", "cs":
 		return "csharp.svg"
-	case "c", "clojure", "haskell", "java", "kotlin", "lua", "php":
+	case "c", "clojure", "dart", "elixir", "groovy", "haskell", "java", "kotlin", "lua", "php":
 		return lg + ".svg"
 	case "cpp", "c++":
 		return "cpp.svg"
