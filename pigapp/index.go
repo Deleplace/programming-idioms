7c88d2e845ef02ccc82ee1c745bb37d2662b335d
Deleplace <deleplace2015@gmail.com>
2022-01-31 10:13:37 +0100

    Bumped ThemeDate.

--- pigapp/index.go
+++ pigapp/index.go
@@ -18,7 +18,7 @@ const ThemeVersion = "default"
 
 // ThemeDate is the prefix used for "revving" the static files and enable long-term HTTP cache.
 // It MUST end with underscore _ (see app.yaml)
-const ThemeDate = "20220127_"
+const ThemeDate = "20220131_"
 
 var r = mux.NewRouter()
 
