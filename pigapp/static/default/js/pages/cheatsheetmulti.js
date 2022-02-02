b615b0c492ece5f94937a1a1a8adbd8557175a53
Deleplace <deleplace2015@gmail.com>
2022-02-02 09:52:02 +0100

    JS code split: move cheatsheets specific code.

--- pigapp/static/default/js/pages/cheatsheetmulti.js
+++ pigapp/static/default/js/pages/cheatsheetmulti.js
@@ -1,5 +1,130 @@
 $(function() {
 
+	function applyCheatsheetFilters() {
+		var word = $("#filter").val();
+		var restrictorLangs = new Set();
+		$("input.restrict-having").each(function(){
+			var lang = $(this).attr("data-lang")
+			if(!lang)
+				return;
+			var checked = $(this).is(':checked');
+			if(!checked)
+				return;
+			restrictorLangs.add(lang);
+		});
+
+		$("tr.cheatsheet-line").each(function(){
+			var line = $(this);
+			var show = true;
+		
+			// Full-text (raw, no tokenization)
+			if(word){
+				var lowerHtml = line.html().toLowerCase();
+				var lowerWord = word.toLowerCase();
+				if( lowerHtml.indexOf(lowerWord) === -1 ){
+					show = false;
+				}
+			}
+
+			// Restrict to existing impls
+			restrictorLangs.forEach(function(lang) {
+				var cell = line.find("td.lang-" + lang);
+				if(cell.length==0) {
+					console.log("Table cell for " + lang + " not found!");
+					return;
+				}
+				if(cell.text().trim() === "") {
+					// No impl in this language, let's hide the whole line
+					show = false;
+				}
+			});
+
+			if(show) {
+				line.show('normal');
+			} else {
+				line.hide('normal');
+			}
+		});
+	}
+
+	$("button.page-print").on("click", function(){
+		using("print");
+		window.print();
+	});
+
+	$(".cheatsheet-lines button.close").on("click", function(){
+		var line = $(this).closest("tr");
+		var idiomID = line.find("th.idiom-id").text();
+		line.remove();	
+		using("cheatsheet/remove-line/" + idiomID);
+	});
+
+	$(".page-cheatsheet #showIdiomId").on("change", function(){
+		if( $(this).is(':checked') ){
+			$("th.idiom-id").show();
+			using("cheatsheet/options/idiom-id/show");
+		}else{
+			$("th.idiom-id").hide();
+			using("cheatsheet/options/idiom-id/hide");
+		}
+	});
+
+	$(".page-cheatsheet #showImports").on("change", function(){
+		if( $(this).is(':checked') ){
+			$(".piimports").show();
+			using("cheatsheet/options/imports/show");
+		}else{
+			$(".piimports").hide();
+			using("cheatsheet/options/imports/hide");
+		}
+	});
+
+	$(".page-cheatsheet #showComments").on("change", function(){
+		if( $(this).is(':checked') ){
+			$(".impl-comment").show();
+			using("cheatsheet/options/comments/show");
+		}else{
+			$(".impl-comment").hide();
+			using("cheatsheet/options/comments/hide");
+		}
+	});
+
+	$(".page-cheatsheet #showExternalLinks").on("change", function(){
+		if( $(this).is(':checked') ){
+			$(".impl-external-links").show();
+			using("cheatsheet/options/impl-external-links/show");
+		}else{
+			$(".impl-external-links").hide();
+			using("cheatsheet/options/impl-external-links/hide");
+		}
+	});
+
+	$(".page-cheatsheet #filter").on("change", function(){
+		applyCheatsheetFilters();
+		var word = $("#filter").val();
+		using("cheatsheet/options/filter/" + word); // spaces will appear as %20
+	});
+
+	$("input.restrict-having").on("change", function(){
+		applyCheatsheetFilters();
+		var lang = $(this).attr("data-lang");
+		if( $(this).val() )
+			using("cheatsheet/restrict/" + lang);
+		else
+			using("cheatsheet/unrestrict/" + lang);
+	});
+
+	//
+	// On page load:
+	//
+    
+	// #134 Auto add favorite languages
+	var path = window.location.pathname;
+	path = path.substring("/cheatsheet/".length);
+	let langs = path.split(/\//);
+	console.log("Adding favlangs", langs);
+	addFavlangsInCookie(langs);
+
     // #186 In Firefox, checkboxes may come already checked from last visit
 	if( $(".page-cheatsheet #showImports").is(':checked') ){
 		$(".piimports").show();
@@ -10,5 +135,7 @@ $(function() {
 	if( $(".page-cheatsheet #showExternalLinks").is(':checked') ){
 		$(".impl-external-links").show();
     }
-    
+
+	// Filter out lines "assumed uninteresting"
+	applyCheatsheetFilters();
 });
--- pigapp/static/default/js/programming-idioms.js
+++ pigapp/static/default/js/programming-idioms.js
@@ -412,45 +412,7 @@ $(function() {
 			$.cookie("see-non-favorite", "1", { expires : 100, path: '/' });
 		}
 	};
-
-	function hasFavlangInCookie(lg) {
-		lg = normLang(lg);
-		let cookielangs = $.cookie("my-languages");
-		if(!cookielangs)
-			return false;
-		let favlangsConcat = cookielangs.toLowerCase();
-		let favlangs = favlangsConcat.split("_");
-		return favlangs.indexOf(lg.toLowerCase()) !== -1;
-	}
-	
-	var normLang = function(lang){
-		switch(lang.toLowerCase()){
-		case "c++":
-			return "Cpp";
-		case "c#":
-			return "Csharp";
-		case "cs":
-			return "Csharp";
-		case "golang":
-			return "go";
-		case "py":
-			return "Python";
-		case "rs":
-			return "Rust";
-		}
-		return lang;
-	}
-
-	var niceLang = function(lang){
-		switch(lang){
-		case "Cpp":
-			return "C++";
-		case "Csharp":
-			return "C#";
-		}
-		return lang;
-	}
-	
+		
 	$(document).on('click', '.show-languages-pool', function(){
 		$('.addible-languages-pool').show(200);
 	});
@@ -524,11 +486,6 @@ $(function() {
 		let lang = parts[parts.length-1];
 		lang = capitalizeFirstLetter(lang);
 		return lang;
-	}
-
-	function capitalizeFirstLetter(string) {
-		// https://stackoverflow.com/a/1026087
-		return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 
 	if( isIdiomDetailWithLang() ) {
@@ -1023,140 +980,6 @@ $(function() {
 		return false;
 	});
 
-	//
-	// Cheatsheet (printable) page
-	//
-	$("button.page-print").on("click", function(){
-		using("print");
-		window.print();
-	});
-
-	$(".cheatsheet-lines button.close").on("click", function(){
-		var line = $(this).closest("tr");
-		var idiomID = line.find("th.idiom-id").text();
-		line.remove();	
-		using("cheatsheet/remove-line/" + idiomID);
-	});
-
-	$(".page-cheatsheet #showIdiomId").on("change", function(){
-		if( $(this).is(':checked') ){
-			$("th.idiom-id").show();
-			using("cheatsheet/options/idiom-id/show");
-		}else{
-			$("th.idiom-id").hide();
-			using("cheatsheet/options/idiom-id/hide");
-		}
-	});
-
-	$(".page-cheatsheet #showImports").on("change", function(){
-		if( $(this).is(':checked') ){
-			$(".piimports").show();
-			using("cheatsheet/options/imports/show");
-		}else{
-			$(".piimports").hide();
-			using("cheatsheet/options/imports/hide");
-		}
-	});
-
-	$(".page-cheatsheet #showComments").on("change", function(){
-		if( $(this).is(':checked') ){
-			$(".impl-comment").show();
-			using("cheatsheet/options/comments/show");
-		}else{
-			$(".impl-comment").hide();
-			using("cheatsheet/options/comments/hide");
-		}
-	});
-
-	$(".page-cheatsheet #showExternalLinks").on("change", function(){
-		if( $(this).is(':checked') ){
-			$(".impl-external-links").show();
-			using("cheatsheet/options/impl-external-links/show");
-		}else{
-			$(".impl-external-links").hide();
-			using("cheatsheet/options/impl-external-links/hide");
-		}
-	});
-
-	$(".page-cheatsheet #filter").on("change", function(){
-		applyCheatsheetFilters();
-		var word = $("#filter").val();
-		using("cheatsheet/options/filter/" + word); // spaces will appear as %20
-	});
-
-	$("input.restrict-having").on("change", function(){
-		applyCheatsheetFilters();
-		var lang = $(this).attr("data-lang");
-		if( $(this).val() )
-			using("cheatsheet/restrict/" + lang);
-		else
-			using("cheatsheet/unrestrict/" + lang);
-	});
-
-	function applyCheatsheetFilters() {
-		var word = $("#filter").val();
-		var restrictorLangs = new Set();
-		$("input.restrict-having").each(function(){
-			var lang = $(this).attr("data-lang")
-			if(!lang)
-				return;
-			var checked = $(this).is(':checked');
-			if(!checked)
-				return;
-			restrictorLangs.add(lang);
-		});
-
-		$("tr.cheatsheet-line").each(function(){
-			var line = $(this);
-			var show = true;
-		
-			// Full-text (raw, no tokenization)
-			if(word){
-				var lowerHtml = line.html().toLowerCase();
-				var lowerWord = word.toLowerCase();
-				if( lowerHtml.indexOf(lowerWord) === -1 ){
-					show = false;
-				}
-			}
-
-			// Restrict to existing impls
-			restrictorLangs.forEach(function(lang) {
-				var cell = line.find("td.lang-" + lang);
-				if(cell.length==0) {
-					console.log("Table cell for " + lang + " not found!");
-					return;
-				}
-				if(cell.text().trim() === "") {
-					// No impl in this language, let's hide the whole line
-					show = false;
-				}
-			});
-
-			if(show) {
-				line.show('normal');
-			} else {
-				line.hide('normal');
-			}
-		});
-
-	}
-
-	function addFavlangsInCookie(langs) {
-		var langsConcat = $.cookie("my-languages") || "_";
-		var newLangsConcat = langsConcat;
-		for (var i = 0; i<langs.length; i++) {
-			var lang = langs[i];
-			lang = capitalizeFirstLetter(lang);
-			lang = normLang(lang);
-			if(!hasFavlangInCookie(lang)) {
-				newLangsConcat += lang + "_";
-			}
-		}
-		if(newLangsConcat != langsConcat){
-			$.cookie("my-languages", newLangsConcat,{ expires : 100, path: '/' });
-		}
-	}
-
 	// Restore a previous version of the Idiom
 	// (only the admin can do this)
 	$("form.idiom-restore-version > input.presubmit").on("click", function(e) {
@@ -1171,21 +994,6 @@ $(function() {
 		form.find("input[name=why]").val(reason);
 		form.submit();
 	});
-
-	function isCheatsheetPage() {
-		return /\/cheatsheet\//.test(window.location.pathname);
-	}
-
-	if( isCheatsheetPage() ) {
-		// #134 Auto add favorite languages
-		var path = window.location.pathname;
-		path = path.substring("/cheatsheet/".length);
-		let langs = path.split(/\//);
-		console.log("Adding favlangs", langs);
-		addFavlangsInCookie(langs);
-		// Filter out lines "assumed uninteresting"
-		applyCheatsheetFilters();
-	}
 
 	function generateQAStructuredData(idiom) {
 		let lang = extractLangFromURL();
@@ -1277,4 +1085,63 @@ function using(what) {
 			page: window.location.pathname+window.location.search
 		})
 	});
+}
+
+function normLang(lang){
+	switch(lang.toLowerCase()){
+	case "c++":
+		return "Cpp";
+	case "c#":
+		return "Csharp";
+	case "cs":
+		return "Csharp";
+	case "golang":
+		return "go";
+	case "py":
+		return "Python";
+	case "rs":
+		return "Rust";
+	}
+	return lang;
+}
+
+function niceLang(lang){
+	switch(lang){
+	case "Cpp":
+		return "C++";
+	case "Csharp":
+		return "C#";
+	}
+	return lang;
+}
+
+function capitalizeFirstLetter(string) {
+	// https://stackoverflow.com/a/1026087
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function hasFavlangInCookie(lg) {
+	lg = normLang(lg);
+	let cookielangs = $.cookie("my-languages");
+	if(!cookielangs)
+		return false;
+	let favlangsConcat = cookielangs.toLowerCase();
+	let favlangs = favlangsConcat.split("_");
+	return favlangs.indexOf(lg.toLowerCase()) !== -1;
+}
+
+function addFavlangsInCookie(langs) {
+	var langsConcat = $.cookie("my-languages") || "_";
+	var newLangsConcat = langsConcat;
+	for (var i = 0; i<langs.length; i++) {
+		var lang = langs[i];
+		lang = capitalizeFirstLetter(lang);
+		lang = normLang(lang);
+		if(!hasFavlangInCookie(lang)) {
+			newLangsConcat += lang + "_";
+		}
+	}
+	if(newLangsConcat != langsConcat){
+		$.cookie("my-languages", newLangsConcat,{ expires : 100, path: '/' });
+	}
 }
