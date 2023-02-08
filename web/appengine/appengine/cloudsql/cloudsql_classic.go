c95d1d2470b52c7d16c25b5b1caeb45941466264
Deleplace <deleplace2015@gmail.com>
2023-02-08 11:17:22 +0100

    App Engine runtime go119: modified appengine bundled services libs (added Search).

---
+++ web/appengine/appengine/cloudsql/cloudsql_classic.go
@@ -0,0 +1,18 @@
+// Copyright 2013 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+//go:build appengine
+// +build appengine
+
+package cloudsql
+
+import (
+	"net"
+
+	"appengine/cloudsql"
+)
+
+func connect(instance string) (net.Conn, error) {
+	return cloudsql.Dial(instance)
+}
